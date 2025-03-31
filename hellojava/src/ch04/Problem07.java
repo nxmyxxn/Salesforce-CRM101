63fb7199788cee4447eabfac5f28b785426b922f
nxmyxxn <m9651m@naver.com>
2025-03-31 10:15:23 +0900

    correct while loop logic

--- hellojava/src/ch04/Problem07.java
+++ hellojava/src/ch04/Problem07.java
@@ -10,36 +10,42 @@ public class Problem07 {
 		System.out.println("=========================");
 
 		Scanner sc = new Scanner(System.in);
-		
-		int answer = (int) (Math.random()*99);
+		boolean play = true;
 		String c = "";
 		
-		while(true) {
-			System.out.println(">>");
-			int input = sc.nextInt();
-			if(input>answer)
-				System.out.println("더 낮게");
-			else if(input<answer)
-				System.out.println("더 높게");
-			else {
-				System.out.print("맞았습니다.\n"+"게임을 종료하시겠습니까?(y/n) >>");
-				sc.nextLine();
-				c= sc.nextLine();
-				if(c.equals("y")) {
-					System.out.println("=========================");
-					System.out.println("     [숫자맞추기게임 종료]     ");
-					System.out.println("=========================");	
+		while( play ==true) {
+			// 게임 진행 여부
+			int answer = (int) (Math.random()*99);
+			System.out.println(answer);
+			
+			while(true) {			
+				System.out.println(">>");
+				int input = sc.nextInt();
+				
+				if(input>answer)
+					System.out.println("더 낮게");
+				else if(input<answer)
+					System.out.println("더 높게");
+				else {
+					System.out.print("맞았습니다.");
 					break;
 				}
-				else if(c.equals("n")){
-					continue;
-				}
-				
 			}
-				
+
+			System.out.println("게임을 종료하시겠습니까?(y/n) >>");		
+			sc.nextLine();
+			c= sc.nextLine();
 			
-		}
-		
+			if("y".equals(c) || "Y".equals(c) ) {
+				play = false;
+			}
+			else if("n".equals(c)){
+				play = true;
+			}					
+		}	
+		System.out.println("=========================");
+		System.out.println("     [숫자맞추기게임 종료]     ");
+		System.out.println("=========================");	
 	}
 
 }
